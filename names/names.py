0acdcbbaa1f84c66775767a1b795189a63c86461
Axolotl <samuelmachat@gmail.com>
2019-03-01 13:15:39 -0500

    add set solution

--- names/names.py
+++ names/names.py
@@ -19,56 +19,61 @@ f.close()
 #  set up binary tree class
 
 
-class BinarySearchTree:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
+# class BinarySearchTree:
+#     def __init__(self, value):
+#         self.value = value
+#         self.left = None
+#         self.right = None
 
-    def insert(self, value):
-        # print(f'Adding: {value}')
-        new_tree = BinarySearchTree(value)
-        if (value < self.value):
-            if not self.left:
-                self.left = new_tree
-            else:
-                self.left.insert(value)
-        elif value >= self.value:
-            if not self.right:
-                self.right = new_tree
-            else:
-                self.right.insert(value)
+#     def insert(self, value):
+#         # print(f'Adding: {value}')
+#         new_tree = BinarySearchTree(value)
+#         if (value < self.value):
+#             if not self.left:
+#                 self.left = new_tree
+#             else:
+#                 self.left.insert(value)
+#         elif value >= self.value:
+#             if not self.right:
+#                 self.right = new_tree
+#             else:
+#                 self.right.insert(value)
 
-    def contains(self, target):
-        # print(f'Contains: {target}')
-        if self.value == target:
-            return True
-        if self.value > target:
-            if self.left and self.left.contains(target):
-                return True
-            else:
-                return False
-        elif self.value < target:
-            if self.right and self.right.contains(target):
-                return True
-            else:
-                return False
+#     def contains(self, target):
+#         # print(f'Contains: {target}')
+#         if self.value == target:
+#             return True
+#         if self.value > target:
+#             if self.left and self.left.contains(target):
+#                 return True
+#             else:
+#                 return False
+#         elif self.value < target:
+#             if self.right and self.right.contains(target):
+#                 return True
+#             else:
+#                 return False
 
 
-#  initialize tree
-tree = BinarySearchTree(names_1[0])
+# #  initialize tree
+# tree = BinarySearchTree(names_1[0])
 
-#  add all names from names_1 to tree
-for name in names_1:
-    tree.insert(name)
+# #  add all names from names_1 to tree
+# for name in names_1:
+#     tree.insert(name)
 
-#  check if each name in names_2 is already in tree
+# #  check if each name in names_2 is already in tree
+# duplicates = []
+
+# for name in names_2:
+#     if tree.contains(name):
+#         duplicates.append(name)
+
 duplicates = []
-
+names_1_set = set(names_1)
 for name in names_2:
-    if tree.contains(name):
+    if name in names_1_set:
         duplicates.append(name)
-
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
